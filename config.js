8e78dcf0d376e29c272ae148a13e5f172e899788
khanhnef <86770769+khanhnef@users.noreply.github.com>
2021-07-01 15:47:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Tró hâm hâm kia!',
+    introDesc: `Anh dỗi em phải không
+    Ai cho anh dỗi em hả???`,
+    btnIntro: 'Hứ',
+    title: 'Vì sao dỗi em hả? 😙',
+    desc: 'Trả lời đúng sự thật không t đấm ',
+    btnYes: 'Vì em quá cutee ahihi <33',
+    btnNo: 'Ứ thèm dỗi -.- ',
+    question:'Anh có yêu em hông trả lời mau...',
+    btnReply: 'Gửi cho em bé <3',
+    reply: 'Yêu em bé nhất trên đời <3',
+    mess: 'Tui biết thừa 🥰. Yêu tró nhiều nhiều. Nghiêm túc đó',
+    messDesc: 'Hết dỗi t mau.',
+    btnAccept: 'ấu kề <3',
+    messLink: 'https://m.me/dieukhanh.nguyen.9256' 
 }
